a5421ee1fe38aeb7069e8d4d964f8c88245aba96
adirajsuryvanshi <132834422+adirajsuryvanshi@users.noreply.github.com>
2024-02-02 17:24:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, SafeAreaView, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
+        Local files and assets can be imported by dragging and dropping them into the editor
+      </Text>
+      <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 24,
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  logo: {
+    height: 128,
+    width: 128,
+  },
+});
